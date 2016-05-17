6eef04a90f6a7c2476874790131e08919832ab51
AlkBur <alkboor@mail.ru>
2016-05-17 14:06:03 +0300

    Added changes

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,4 +19,9 @@ from django.contrib import admin
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^question/', include('qa.urls')),
+    url(r'^$', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
 ]
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.test, name='question'),
+    url(r'^$', views.test, name='other'),
     url(r'(?P<id>[0-9]+)/$', views.test, name='question'),
 ]
