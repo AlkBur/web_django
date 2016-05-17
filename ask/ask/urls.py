4a0556b886502a233adb419806f6663bcca51867
AlkBur <alkboor@mail.ru>
2016-05-17 14:13:45 +0300

    Added changes

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^ask/', include('qa.urls')),
     url(r'^popular/', include('qa.urls')),
     url(r'^new/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
 ]
