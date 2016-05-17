1e956d24ca7165916e4fdcd33c434efbbd24c376
AlkBur <alkboor@mail.ru>
2016-05-17 15:06:08 +0300

    Added changes

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test, name='other'),
+    #url(r'^/$', views.test, name='other'),
     url(r'(?P<id>[0-9]+)/$', views.test, name='question'),
 ]
